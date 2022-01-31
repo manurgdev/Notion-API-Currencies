fa84893d1d0f809517f381c82e54a9ff0b89ebe5
manurgdev <manuelrodriguezgil@informaticos.com>
2022-01-31 02:37:16 +0100

    Updated repository with my own solution. - Udpated README.md - Uploaded img - Changed .env_dist to put EUR as default currency

--- README.md
+++ README.md
@@ -1,20 +1,62 @@
 # Notion API - Crypto/Foreign Currency Prices
 
+Based on the original repository from [Broto](https://github.com/brotoo25)/[Notion-API-Currencies](https://github.com/brotoo25/Notion-API-Currencies)
+
+This repo is an update to the latest integration for Notion API totally made in JavaScript.
+
+## Breaking Changes / Change log
+* Removed the "updateCurrencyConversions" method cause it's not needed for me.
+* Changed all the process to obtain the current cryptos from the Notion DB and then update them.
+* Got available cryptos standalone to avoid repeated updates.
+* Changed DB column name from "USD" to "Current" cause I want to obtain current value for each crypto instead of it's value on USD.
+
+## Why I made this?
+I want to get noticed for the all value changes on my crypto investments.  First i created my own Notion DB and then I started to search a method to update the current prices in my local currency (EUR)
+
+I found this repo and I started to make changes to fit them to my DB and i ended with this solution.
+
+This fork is for persons who follow an DCA investment strategy and don't want to waste time updating values from a table.
+
+## Current DB
+
+I leave here an image of my current DB
+
+![screenshot image](/img/current-db-schema.png)
+
+## DB Formulas
+
+As you can see I setted a lot of formulas in my DB. I leave them below:
+
+* "Status" (to see if you get gain or loss)
+    * `(prop("Gain EUR") >= 0 or prop("Crypto") == "USDT") ? "🟢" : "🔴"`
+* "Real Amount" (real amount obtained of the crypto at the moment of the investment)
+    * `toNumber(slice(format(prop("Coin Amount") * prop("Coin Price")), 0, 5))`
+* "% Gain" (Percentage of your gain/loss)
+    * `toNumber(slice(format((prop("Current") / prop("Coin Price") - 1) * 100), 0, 4)) / 100`
+* "Gain EUR" (Gain/loss in EUR)
+    * `prop("% Gain") * prop("Real Amount") + prop("Real Amount") - prop("Invested")`
+
+NOTE: I used EUR, but you can put your local currency in all the formulas and fields.
+
+## Important fields
+* **Crypto_ID**: ID of the crypto to query on CoinGecko API
+* **Current**: Field used to update the current value of each crypto on your local currency
+
 ## Project Description:
 [Medium Post](https://medium.com/@abraaorl/managing-financial-assets-with-notions-api-4945616d64f5)
 
 ## Setup:
 
 ### Notion Credentials
-Replace the content of the '.env' file with your own.
+Create a `.env` file with the content of the `.env_dist` file with your own.
 
-### Intall 
+### Install 
 
 ```
-npm install
+yarn install
 ```
 
 ## Run:
 ```
-node start
+yarn start
 ```
--- index.js
+++ index.js
@@ -1,61 +1,59 @@
-const axios = require('axios')
-const cron = require('node-cron')
-const dotenv = require('dotenv').config()
-const { Client } = require('@notionhq/client')
+const axios = require( 'axios' )
+const cron = require( 'node-cron' )
+const dotenv = require( 'dotenv' ).config()
+const https = require( 'https' );
+const { Client } = require( '@notionhq/client' )
 
-const axiosClient = axios.create({
+const axiosClient = axios.create( {
   timeout: 160000,
   maxContentLength: 500 * 1000 * 1000,
-  httpsAgent: new https.Agent({ keepAlive: true }),
-})
+  httpsAgent: new https.Agent( { keepAlive: true } ),
+} )
 
-const notion = new Client({
+const notion = new Client( {
   auth: process.env.NOTION_TOKEN,
-})
+} )
 
 const databaseId = process.env.NOTION_DATABASE_ID
 const defaultCurrency = process.env.DEFAULT_CURRENCY
 
 const refreshDatabase = async () => {
   const payload = {
-    path: `databases/${databaseId}/query`,
-    method: 'POST'
+    database_id: process.env.NOTION_DATABASE_ID
   }
 
-  const { results } = await notion.request(payload)
-  updateCryptoConversions(results)
-  updateCurrencyConversions(results)
+  const { results } = await notion.databases.query( payload );
+  let cryptos = results.map( result => {
+    return { page_id: result.id, name: result.properties.Crypto_ID.select.name };
+  } )
+
+  if ( cryptos ) {
+    updateCryptoConversions( cryptos );
+  }
 }
 
-async function updateCryptoConversions(notionPages) {
-  notionPages.map(async (page) => {
-    const coinType = page.properties.Crypto_ID.rich_text[0]?.text.content || "EMPTY"
-    if (coinType != "EMPTY") {
-      const cryptoValue = await fetchPriceOnCoinGecko(coinType, defaultCurrency)
-      _updateNotionTable(page.id, cryptoValue)
+async function updateCryptoConversions( cryptos ) {
+  let uniqueCryptos = [...new Set( cryptos.map( crypto => crypto.name ) )];
+  uniqueCryptos.forEach( async ( uniqueCrypto ) => {
+    const coinType = uniqueCrypto || "EMPTY"
+    if ( coinType != "EMPTY" ) {
+      const cryptoValue = await fetchPriceOnCoinGecko( coinType, defaultCurrency )
+      _updateNotionTable( cryptos.filter( crypto => crypto.name === coinType ), cryptoValue )
     }
-  })
+  } )
 }
 
-async function updateCurrencyConversions(notionPages) {
-  notionPages.map(async (page) => {
-    const coinType = page.properties.Currency_ID.rich_text[0]?.text.content || "EMPTY"
-    if (coinType != "EMPTY") {
-      const currencyValue = await fetchCurrencyPrice(coinType, defaultCurrency)
-      _updateNotionTable(page.id, parseFloat(currencyValue))
-    }
-  })
-}
-
-async function _updateNotionTable(pageId, monetaryValue) {
-  notion.pages.update({
-    page_id: pageId,
-    properties: {
-      USD: {
-        number: monetaryValue
+async function _updateNotionTable( pagesToUpdate, cryptoValue ) {
+  pagesToUpdate.forEach( ( { page_id } ) => {
+    notion.pages.update( {
+      page_id,
+      properties: {
+        Current: {
+          number: cryptoValue
+        }
       }
-    }
-  })
+    } )
+  } )
 }
 
 /**
@@ -64,36 +62,16 @@ async function _updateNotionTable(pageId, monetaryValue) {
  *                  https://api.coingecko.com/api/v3/coins/list?include_platform=false
  *         (defaultCurrency) - Base Currency Code to calculate price. Ex: USD, BRL, NZD
  **/
- async function fetchPriceOnCoinGecko(coin, defaultCurrency) {
+async function fetchPriceOnCoinGecko( coin, defaultCurrency ) {
   try {
-    const response = await axiosClient.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${defaultCurrency}`);
+    const response = await axiosClient.get( `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${defaultCurrency}` );
     return response.data[`${coin}`][defaultCurrency.toLowerCase()]
-  } catch (error) {
-    console.error(error);
+  } catch ( error ) {
+    console.error( error );
   }
 }
 
-/**
- * Get most recent price of any currency listed at 'API de Moedas'.
- * https://docs.awesomeapi.com.br/api-de-moedas
- * 
- * Params: (from) - Currency Code. Ex: USD, BRL, NZD
- *         (to)   - Currency Code. Ex: USD, BRL, NZD
- **/
-async function fetchCurrencyPrice(from, to) {
-  try {
-    if (from.toUpperCase() == to.toUpperCase()) {
-      return 1
-    }
-
-    const response = await axiosClient.get(`https://economia.awesomeapi.com.br/json/last/${from.toUpperCase()}-${to}`);
-    return response.data[`${from}${to}`]['high']
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Run the refresh every minute
-cron.schedule('*/30 * * * *', () => {
+// Run the refresh every minute | Change it as you want or remove cron for one-shot refresh
+cron.schedule( '* * * * *', () => {
   refreshDatabase()
-})
+} )
